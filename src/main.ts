b0a0fe30801ad919143d4fd52c825743766bddf2
vishwajeet <vishwajeet.manu@gmail.com>
2025-06-13 23:52:30 +0530

    setup with swagger UI on root

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager API')
+    .setDescription("A simple API to manage user's day to day tasks.")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
+
 bootstrap();
