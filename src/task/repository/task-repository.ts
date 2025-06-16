2bdc8286373db6d37a44a473dfdfe906aa4645a9
vishwajeet <vishwajeet.manu@gmail.com>
2025-06-16 18:52:28 +0530

    added db implementation on postgres

--- src/task/repository/task-repository.ts
+++ src/task/repository/task-repository.ts
@@ -1,25 +1,73 @@
 import { ITaskRepository } from '../interfaces/task-repository.interface';
 import { ITask } from '../interfaces/task.interface';
+import { Inject } from '@nestjs/common';
+import { Pool } from 'pg';
+import { v4 as uuid } from 'uuid';
+
+function mapDbToTask(row: any): ITask {
+  return {
+    id: row.id,
+    name: row.name,
+    dueDate: row.duedate,
+    priority: row.priority,
+    userId: row.userid,     
+  };
+}
+
 
 
 export class TaskRepository implements ITaskRepository {
+  
+  constructor(
+    @Inject('PG_POOL') private readonly pool: Pool, 
+  ) {}
+
   async create(task: ITask): Promise<ITask> {
-    throw new Error('Method not implemented.');
+    const id = uuid();
+    const query = `
+      INSERT INTO tasks (id, name, duedate, priority, userid)
+      VALUES ($1, $2, $3, $4, $5)
+      RETURNING *
+    `;
+    const values = [id, task.name, task.dueDate, task.priority, task.userId];
+    const result = await this.pool.query(query, values);
+    return mapDbToTask(result.rows[0]);
   }
 
   async update(taskId: string, updates: Partial<ITask>): Promise<ITask | null> {
-    throw new Error('Method not implemented.');
+    const fields: string[] = [];
+    const values: (string | number | null)[] = [];
+    let index = 1;
+
+    for (const [key, value] of Object.entries(updates)) {
+      fields.push(`${key} = $${index++}`);
+      values.push(value);
+    }
+
+    const query = `
+      UPDATE tasks
+      SET ${fields.join(', ')}
+      WHERE id = $${index}
+      RETURNING *
+    `;
+
+    values.push(taskId);
+    const result = await this.pool.query(query, values);
+    return result.rowCount ? mapDbToTask(result.rows[0])  : null;
   }
 
   async delete(taskId: string): Promise<void> {
-    throw new Error('Method not implemented.');
+    await this.pool.query('DELETE FROM tasks WHERE id = $1', [taskId]);
   }
 
   async findById(taskId: string): Promise<ITask | null> {
-    throw new Error('Method not implemented.');
+    const result = await this.pool.query('SELECT * FROM tasks WHERE id = $1', [taskId]);
+    return  result.rowCount ? mapDbToTask(result.rows[0]) : null;
   }
 
   async findByUser(userId: string): Promise<ITask[]> {
-    throw new Error('Method not implemented.');
+    const result = await this.pool.query("SELECT id, name, TO_CHAR(duedate, 'YYYY-MM-DD') AS duedate, priority,userid FROM tasks WHERE userid = $1", [userId]);
+    const tasks = result.rows.map(mapDbToTask);
+    return tasks as ITask[];
   }
 }
--- src/user/repositories/user-repository.ts
+++ src/user/repositories/user-repository.ts
@@ -1,20 +1,53 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, Inject} from "@nestjs/common";
 import { IUserRepository } from "../interfaces/user-repository.interface";
 import { IUser } from "../interfaces/user.interface";
 
+import { v4 as uuid } from 'uuid';
+import { Pool } from 'pg';
+
+export function mapDbToUser(row: any): IUser {
+  return {
+    id: row.id,
+    email: row.email,
+    firstName: row.firstname,
+    lastName: row.lastname,
+    password: row.password
+  };
+}
+
 @Injectable()
 export class UserRepository implements IUserRepository {
-    // constructor(@InjectRepository(UserEntity) private repo: Repository<UserEntity>) {}
     
-    async findById(id: string): Promise<IUser | undefined> {
-        throw new Error("Method not implemented.");
-    }
-
-    async create(data: Omit<IUser, "id">): Promise<IUser> {
-        throw new Error("Method not implemented.");
-    }
+    constructor(
+    @Inject('PG_POOL') private readonly pool: Pool
+  ) {}
 
     async findByEmail(email: string): Promise<IUser | undefined> {
-        throw new Error("Method not implemented.");
-    }
+    const result = await this.pool.query(
+      'SELECT * FROM users WHERE email = $1 LIMIT 1',
+      [email]
+    );
+    return result.rows[0] ?  mapDbToUser(result.rows[0]) : undefined;
+  }
+
+  async findById(id: string): Promise<IUser | undefined> {
+    const result = await this.pool.query(
+      'SELECT * FROM users WHERE id = $1 LIMIT 1',
+      [id]
+    );
+    return result.rows[0] ? mapDbToUser(result.rows[0]) : undefined;
+  }
+
+  async create(data: Omit<IUser, 'id'>): Promise<IUser> {
+    const id = uuid();
+    const { email, password, firstName, lastName } = data;
+
+    await this.pool.query(
+      `INSERT INTO users (id, email, password, firstname, lastname)
+       VALUES ($1, $2, $3, $4, $5)`,
+      [id, email, password, firstName, lastName]
+    );
+
+    return { id, ...data };
+  }
 }
