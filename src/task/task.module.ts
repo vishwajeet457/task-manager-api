7279fddee009a1a4168d95abf28c2f00bc367753
vishwajeet <vishwajeet.manu@gmail.com>
2025-06-16 11:03:29 +0530

    added apis

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,4 +1,34 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JsonTaskRepository } from './repository/json-task-repository';
+import { TaskRepository } from './repository/task-repository';
+import { TaskService } from 'src/task/task.service';
+import { TaskController } from './task.controller';
 
-@Module({})
-export class TaskModule {}
+const taskRepoProvider: Provider = {
+  provide: 'ITaskRepository',
+  useFactory: (config: ConfigService) => {
+    const dbMode = config.get('DB_MODE');
+    if (dbMode === 'json') {
+      return new JsonTaskRepository();
+    }else{
+      return new TaskRepository();
+    }
+  },
+  inject: [ConfigService],
+};
+
+@Module({
+    controllers: [TaskController],
+    providers: [
+      {
+        provide: 'ITaskService',
+        useClass: TaskService,
+      },
+      taskRepoProvider
+    ],
+    exports: [],
+})
+export class TaskModule {
+
+}
